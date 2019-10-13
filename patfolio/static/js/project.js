70b8d84b9632a7323cb529c8cf262f5d28bcb779
Patrick Zhang <patdujour@gmail.com>
2019-10-13 14:46:22 -0700

    finish menu breathing open/close

--- patfolio/static/js/project.js
+++ patfolio/static/js/project.js
@@ -1,7 +1,23 @@
 /* Project specific Javascript goes here. */
 $(document).ready(function() {
     $('.navbar-toggler').click(function() {
-        console.log('wtf');
-      $('.nav-overlay').toggleClass('hidden');
+        if($('.nav-overlay').hasClass('hidden')) {
+            $('.nav-overlay').toggleClass('hidden')
+            $('.nav-overlay').toggleClass('opacity-0')
+            $('.nav-overlay').toggleClass('opacity-1')
+            $('.nav-overlay').toggleClass('hiding');
+            setTimeout(function() {
+                $('.nav-overlay').toggleClass('hiding');
+            }, 1000);
+        }
+        else {
+            $('.nav-overlay').toggleClass('hiding');
+            $('.nav-overlay').toggleClass('opacity-1');
+            $('.nav-overlay').toggleClass('opacity-0');
+            setTimeout(function() {
+                $('.nav-overlay').toggleClass('hidden');
+                $('.nav-overlay').toggleClass('hiding');
+            }, 1000);
+        }
     });
 });
